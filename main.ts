a8f478d4835caa290d529ff842ffe19d48c2255a
hmauroy <hmauroy@gmail.com>
2024-03-17 21:32:22 +0100

    Testing to see if i2c is rerouted through pins 1 and 2.

--- main.ts
+++ main.ts
@@ -1,15 +1,14 @@
 i2crr.setI2CPins(DigitalPin.P2, DigitalPin.P1)
-while (true) {
-    pins.i2cWriteNumber(
-    24,
-    3,
-    NumberFormat.Int8LE,
-    false
-    )
-    pins.i2cWriteNumber(
-    24,
-    1,
-    NumberFormat.Int8LE,
-    false
-    )
-}
+basic.forever(function () {
+    while (true) {
+        pins.i2cWriteNumber(
+        24,
+        3,
+        NumberFormat.UInt8BE,
+        true
+        )
+        basic.showIcon(IconNames.Heart)
+        basic.pause(100)
+        basic.showIcon(IconNames.Rollerskate)
+    }
+})
