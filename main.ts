ba8d8105620048a637066a5ea0a32f25fe9e78ea
hmauroy <hmauroy@gmail.com>
2024-03-19 18:53:54 +0100

    chatGPT port of Arduino code. Does not work either. Need to check another sensor that gives data back more easily.

--- main.ts
+++ main.ts
@@ -1,41 +1,72 @@
-i2crr.setI2CPins(DigitalPin.P2, DigitalPin.P1)
-basic.showIcon(IconNames.Heart)
-// 0x18
-// Configuration register for Ports, hex: 0x03.
-pins.i2cWriteNumber(
-24,
-3,
-NumberFormat.UInt8BE,
-true
-)
-basic.pause(1)
-// Configures io ports: 0=output, 1=input
-// 0b11011011, Set IO2 and IO5 as outputs, all other pins as inputs. 0=output, 1=input
-pins.i2cWriteNumber(
-24,
-219,
-NumberFormat.UInt8BE,
-true
-)
-basic.pause(1)
-let verdi = pins.i2cReadNumber(0, NumberFormat.Int8LE, false)
-basic.pause(1)
-basic.forever(function () {
-    // 0b00000100); // Decimal 4. Set IO2 high and IO5 low, all other pins unchanged, (io7 io6 io5 io4 io3 io2 io1 io0)
-    // Turn IO2 LED on, and IO5 LED off
+function configureOutput () {
     pins.i2cWriteNumber(
-    24,
-    4,
+    PCA9577_ADDR,
+    CONFIGURATION_MODE,
     NumberFormat.UInt8BE,
     false
     )
-    basic.pause(500)
-    // Turn IO2 LED off, and IO5 LED on.
-    // 0b00100000
+    // Decimal 219. Set IO2 and IO5 as outputs, all other pins as inputs. 0=output, 1=input
     pins.i2cWriteNumber(
-    24,
+    PCA9577_ADDR,
+    219,
+    NumberFormat.UInt8BE,
+    true
+    )
+}
+function turnOffLEDs () {
+    pins.i2cWriteNumber(
+    PCA9577_ADDR,
+    OUTPUT_REGISTER,
+    NumberFormat.UInt8BE,
+    false
+    )
+    // Decimal 32. Set io2 low and io5 high
+    pins.i2cWriteNumber(
+    PCA9577_ADDR,
     32,
     NumberFormat.UInt8BE,
+    true
+    )
+}
+function turnOnLEDs () {
+    pins.i2cWriteNumber(
+    PCA9577_ADDR,
+    OUTPUT_REGISTER,
+    NumberFormat.UInt8BE,
     false
     )
+    // Decimal 4. Set IO2 high and IO5 low, all other pins unchanged, (io7 io6 io5 io4 io3 io2 io1 io0)
+    pins.i2cWriteNumber(
+    PCA9577_ADDR,
+    4,
+    NumberFormat.UInt8BE,
+    true
+    )
+}
+let OUTPUT_REGISTER = 0
+let CONFIGURATION_MODE = 0
+let PCA9577_ADDR = 0
+let led2_state = false
+i2crr.setI2CPins(DigitalPin.P2, DigitalPin.P1)
+// Address of PCA9577 chip
+PCA9577_ADDR = 24
+// Configuration register: which mode is pins operated at?
+CONFIGURATION_MODE = 3
+OUTPUT_REGISTER = 1
+let led1_state = true
+// Setup
+configureOutput()
+// Loop
+basic.forever(function () {
+    if (led1_state == true) {
+        // Turn off LEDs connected to IO2 and IO5
+        turnOffLEDs()
+    } else {
+        // Turn on LEDs connected to IO2 and IO5
+        turnOnLEDs()
+    }
+    // delay
+    basic.pause(500)
+    // Toggle state
+    led1_state = !(led1_state)
 })
