41daceb90a310f26b205f8ee230d09b0b78c20a9
hmauroy <hmauroy@gmail.com>
2024-03-19 18:49:59 +0100

    Added some pause between i2c writes.

--- main.ts
+++ main.ts
@@ -6,16 +6,20 @@ pins.i2cWriteNumber(
 24,
 3,
 NumberFormat.UInt8BE,
-false
+true
 )
+basic.pause(1)
 // Configures io ports: 0=output, 1=input
 // 0b11011011, Set IO2 and IO5 as outputs, all other pins as inputs. 0=output, 1=input
 pins.i2cWriteNumber(
 24,
 219,
 NumberFormat.UInt8BE,
-false
+true
 )
+basic.pause(1)
+let verdi = pins.i2cReadNumber(0, NumberFormat.Int8LE, false)
+basic.pause(1)
 basic.forever(function () {
     // 0b00000100); // Decimal 4. Set IO2 high and IO5 low, all other pins unchanged, (io7 io6 io5 io4 io3 io2 io1 io0)
     // Turn IO2 LED on, and IO5 LED off
