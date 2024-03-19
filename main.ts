2c940b2f76b6a9cc97cb1128ace73d3975b43d58
hmauroy <hmauroy@gmail.com>
2024-03-19 16:51:25 +0100

    Added comments for how I2C is configured for PCA9557 io expander.

--- main.ts
+++ main.ts
@@ -1,14 +1,28 @@
 i2crr.setI2CPins(DigitalPin.P2, DigitalPin.P1)
+basic.showIcon(IconNames.Heart)
+// 0x18
+// Configuration register for Ports, hex: 0x03.
+pins.i2cWriteNumber(
+24,
+3,
+NumberFormat.UInt8BE,
+false
+)
+// Configures io ports: 0=output, 1=input
+// 0b11011011, Set IO2 and IO5 as outputs, all other pins as inputs. 0=output, 1=input
+pins.i2cWriteNumber(
+24,
+219,
+NumberFormat.UInt8BE,
+false
+)
 basic.forever(function () {
-    while (true) {
-        pins.i2cWriteNumber(
-        24,
-        3,
-        NumberFormat.UInt8BE,
-        true
-        )
-        basic.showIcon(IconNames.Heart)
-        basic.pause(100)
-        basic.showIcon(IconNames.Rollerskate)
-    }
+    basic.pause(100)
+    // 0b00000100); // Decimal 4. Set IO2 high and IO5 low, all other pins unchanged, (io7 io6 io5 io4 io3 io2 io1 io0)
+    pins.i2cWriteNumber(
+    24,
+    4,
+    NumberFormat.UInt8BE,
+    false
+    )
 })
