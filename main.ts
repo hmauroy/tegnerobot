ebd3c2fa303a29da365fe27a294589c16b494b14
hmauroy <hmauroy@gmail.com>
2024-03-25 13:52:38 +0100

    Direct while loop to run pulses. Not testet yet.

--- main.ts
+++ main.ts
@@ -3,5 +3,9 @@ let pca_register = 492
 tegneRobot.setreg()
 pca_register = tegneRobot.getreg()
 basic.showNumber(pca_register)
-pins.analogWritePin(AnalogPin.P0, 1023)
-pins.analogSetPeriod(AnalogPin.P0, 500)
+while (true) {
+    tegneRobot.ledON()
+    tegneRobot.pauseMicroseconds(500)
+    tegneRobot.ledOff()
+    tegneRobot.pauseMicroseconds(500)
+}
