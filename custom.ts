193a6b90fcd1a6fbce90882516bd78d59c3d10cc
hmauroy <hmauroy@gmail.com>
2024-04-22 16:59:06 +0200

    Cleaned up code. Not finished cleaning yet.

--- custom.ts
+++ custom.ts
@@ -11,7 +11,7 @@ enum MyEnum {
     Two,
 }
 
-enum liftPen {
+enum penLifted {
     //% block="lift"
     true,
     //% block="pendown"
@@ -39,7 +39,7 @@ namespace tegneRobot {
      * @param lift if pen should lift after drawing finished
      */
     //% help=circle/draw weight=77
-    //% block="circle|centerX %x|centerY %y|radius %r|liftPen %lift" icon="\uf1db" blockGap=8
+    //% block="circle|centerX %x|centerY %y|radius %r|penLifted %lift" icon="\uf1db" blockGap=8
     //% x.min=0 x.max=16 y.min=0 y.max=12 r.min=0 r.max=8
     //% x.fieldOptions.precision=1 y.fieldOptions.precision=1 
     export function circle(centerX: number, centerY: number, r: number, lift = false): void {
@@ -61,69 +61,13 @@ namespace tegneRobot {
         function turnStepperY() {
             machine.currentPosition.y += machine.direction.y * animation.stepSize;
         }
-     * @param nx: number of steps in x-axis, e.g. 42
-     * @param ny: number of steps in y-axis, e.g. 42
-     * @param timePerStepX: time in microseconds for one step in x-axis, e.g. 2000
-     * @param timePerStepX: time in microseconds for one step in x-axis, e.g. 2000
      */
-    //% help=runSteppers/draw weight=77
-    //% block="Run steppers | nx %nx| ny %ny| timePerStepX %timePerStepX| timePerStepY %timePerStepY" icon="\uf204" blockGap=8
-    
-    export function stepSteppers(nx: number, ny: number, timePerStepX: number, timePerStepY:number): void {
-        let dirX = "CCW"
-        let dirY = "CCW"
-        let pauseX = Math.round(timePerStepX / 2)  // Divide by 2 because step signal is 2 x delayMicroseconds.
-        let pauseY = Math.round(timePerStepY / 2)
-        let dirXpin = 1
-        let dirYpin = 2
-        if (nx > 0) {
-            digitalWrite(dirXpin, true) // Set the spinning direction clockwise (x- and y-axis are wired opposite at the moment).
-            dirX = "CW"
-        }
-        else if (nx < 0) {
-            digitalWrite(dirXpin, false) // CCW
-            dirX = "CCW"
-        }
-        else {
-            dirX = "STILL"
-        }
-        if (ny > 0) {
-            digitalWrite(dirYpin, false) // Set the spinning direction counter clockwise
-            dirY = "CCW"
-        }
-        else if (ny < 0) {
-            digitalWrite(dirYpin, true) // CCW
-            dirY = "CW"
-        }
-        else {
-            dirY = "STILL"
-        }
-        // Takes absolute values of both nsteps and pauseTime.
-        nx = Math.abs(nx)
-        ny = Math.abs(ny)
-        pauseX = Math.abs(pauseX)
-        pauseY = Math.abs(pauseY)
+    //% help=stepSteppers/draw weight=77
+    //% block="Step steppers"  icon="\uf204" blockGap=8
+    export function stepSteppers() {
+        
+       
 
-        let isRunning: boolean = true;
-        let sigX: boolean = true;
-        let sigY: boolean = true;
-        let txLast: number = micros() * 1000; // time in microseconds.
-        let tyLast: number = micros() * 1000;
-        let delta_tx: number = 0;
-        let delta_ty: number = 0;
-
-    }
-
-    /**
-     * TODO: describe your function here
-     * @param x: x-coordinate, e.g. 60
-     * @param x: y-coordinate, e.g. 80
-     */
-    //% block
-    export function moveXY(x: number, y: number): void {
-        for (let i = 0; i < 10; i++) {
-            stepSteppers(42,96,3000,1500);
-        }
     }
 
     //% blockId="setI2CPins" block="set i2c data to %sdaPin and clock to %sclPin|"
@@ -133,6 +77,16 @@ namespace tegneRobot {
     export function setI2CPins(sdaPin: DigitalPin, sclPin: DigitalPin): void {
         // Per https://github.com/microsoft/pxt-microbit/issues/4292
         0;
+    }
+
+    function liftPen() {
+        //% Lifts the pen by moving the servo "upwards"
+        servos.P0.setAngle(150);
+    }
+
+    function lowerPen() {
+        //% Lowers the pen by moving the servo to middle position.
+        servos.P0.setAngle(90);
     }
 
     function digitalWrite(ioPin:number, state: boolean) {
@@ -192,70 +146,6 @@ namespace tegneRobot {
 
         while (input.runningTimeMicros() < endTime) {
             // Wait until desired time has passed
-        }
-    }
-
-    function line_bresenham(x0:number, y0:number, x1:number, y1:number) {
-
-        let dx = Math.abs(x1 - x0);
-        let dy = -Math.abs(y1 - y0);
-        let sx = 0;
-        let sy = 0;
-
-        if (x0 < x1) {
-            sx = 1;
-        } else {
-            sx = -1;
-        }
-
-        if (y0 < y1) {
-            sy = 1;
-        } else {
-            sy = -1;
-        }
-
-        let e = dx + dy;
-        let e2 = 2 * e;
-
-        let isDrawing = true;
-        let teller = 0;
-        while (isDrawing) {
-            teller += 1;
-            if (teller >= 10000) {
-                isDrawing = false;
-            }
-            //console.log(x0, y0);
-            //let p = document.createElement("p");
-            //p.innerHTML = x0 + "," + y0;
-            //output.appendChild(p);
-            // Run steppers one step: x, or y, or both.
-            //changeCellColor(x0, y0);
-            if (x0 === x1 && y0 === y1) {
-                isDrawing = false;
-                break;
-            }
-            // Update error
-            e2 = 2 * e;
-            if (e2 >= dy) {
-                // if reached target x
-                if (x0 === x1) {
-                    isDrawing = false;
-                    break;
-                }
-                // update y-error when x is changed
-                e = e + dy;
-                x0 = x0 + sx;
-            }
-            if (e2 <= dx) {
-                // if reached target y
-                if (y0 === y1) {
-                    isDrawing = false;
-                    break;
-                }
-                // update x-error when y is changed
-                e = e + dx;
-                y0 = y0 + sy;
-            }
         }
     }
 
--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
-let x = 0
 let y = 0
+let x = 0
 let radius = 0.5
-for (let i = 0; i < 6; i++) {
-    tegneRobot.circle(x, y, radius, true)
+for (let index = 0; index < 6; index++) {
+    tegneRobot.circle(
+    x,
+    y,
+    radius,
+    true
+    )
     x += 2
     y += 3
     radius += 0.5
 }
+servos.P0.setAngle(150)
