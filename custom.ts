9504502a103d173795e57ceaa91930e2570303f3
hmauroy <hmauroy@gmail.com>
2024-05-22 12:07:22 +0200

    Implemented i2c read from sd card.

--- custom.ts
+++ custom.ts
@@ -411,6 +411,37 @@ namespace tegneRobot {
         return pins.i2cReadNumber(PCA9557_ADDR, NumberFormat.UInt8BE);
     }
 
+    /*
+    * Request data from Arduino    
+    */
+    //% block "Request Data from Arduino"
+    export function requestData(): void {
+        // Request 32 bytes from the slave device
+        pins.i2cWriteNumber(PCA9557_ADDR, 0, NumberFormat.UInt8LE, true)
+        let receivedData = i2cReadString(PCA9557_ADDR, 32, NumberFormat.UInt8LE)
+
+        serialLog(receivedData);
+
+    }
+
+    /*
+    * Internal function to construct a string from I2C-communication.
+    */
+    function i2cReadString(address: number, maxLength: number, delimiter: number = 0): string {
+        let receivedData = ""
+        let buf = pins.i2cReadBuffer(address, maxLength, false)
+
+        for (let i = 0; i < maxLength; i++) {
+            let charCode = buf.getNumber(NumberFormat.UInt8LE, i)
+            if (charCode == delimiter || charCode == 0) {
+                break
+            }
+            receivedData += String.fromCharCode(charCode)
+        }
+
+        return receivedData
+    }
+
     // Write to register. Write config byte, then sequential write PIN CONFIG. IO5 LOW IO2 HIGH
     //% block
     export function ledON(): void {
