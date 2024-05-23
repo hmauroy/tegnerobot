672c16127fb7b42d914d62ef319fd110e0ad5be6
hmauroy <hmauroy@gmail.com>
2024-05-23 10:30:48 +0200

    Test code for fysikk 1 students

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
 tegneRobot.startDrawing()
-tegneRobot.svgSdCard(false)
+let xpos = 30
+let ypos = 30
+for (let index = 0; index < 5; index++) {
+    tegneRobot.circle(
+    xpos,
+    ypos,
+    15,
+    true
+    )
+    xpos += 10
+    ypos += 5
+}
 tegneRobot.endDrawing()
