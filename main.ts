173eabe96741d7e2b92d2113b59d7f09cbdfada7
hmauroy <hmauroy@gmail.com>
2024-05-24 23:22:58 +0200

    Restore '5/24/2024, 11:14:37 PM Calibration for repetition analysis.'

--- main.ts
+++ main.ts
@@ -1,19 +1,21 @@
+let ypos = 0
+let xpos = 0
 tegneRobot.startDrawing()
 for (let index = 0; index < 3; index++) {
-    tegneRobot.square(
-    5,
-    5,
-    80,
-    0,
-    true
-    )
-}
-for (let index = 0; index < 3; index++) {
-    tegneRobot.circle(
-    50,
-    50,
-    20,
-    true
-    )
+    xpos = 30
+    ypos = 30
+    for (let index = 0; index < 10; index++) {
+        tegneRobot.circle(
+        xpos,
+        ypos,
+        15,
+        true
+        )
+        xpos += 10
+        ypos += 5
+        tegneRobot.moveHeadToMM(100, 90)
+        tegneRobot.lowerPen()
+        tegneRobot.moveHeadToMM(10, 90)
+    }
 }
 tegneRobot.endDrawing()
