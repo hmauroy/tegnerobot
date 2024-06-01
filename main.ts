b12ce3f0160e82cec293d9c852a24417041b0ee8
hmauroy <hmauroy@gmail.com>
2024-06-01 17:46:48 +0200

    Three concentric circles like the first demo video.

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 tegneRobot.startDrawing()
-tegneRobot.circle(
-20,
-30,
-15,
-true
-)
+let x = 60
+let y = 60
+let radius = 30
+for (let index = 0; index < 3; index++) {
+    tegneRobot.circle(
+    x,
+    y,
+    radius,
+    false
+    )
+    radius += 10
+}
 tegneRobot.endDrawing()
